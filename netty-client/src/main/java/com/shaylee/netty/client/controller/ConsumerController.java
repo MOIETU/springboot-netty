c22f17bf77a7b3c34408d78ddb1ab67471af5a59
Adrian <297513931@qq.com>
2020-05-08 17:03:46 +0800

    :art: Improving structure / format of the code.

--- netty-client/src/main/java/com/shaylee/netty/client/controller/ConsumerController.java
+++ netty-client/src/main/java/com/shaylee/netty/client/controller/ConsumerController.java
@@ -3,7 +3,8 @@ package com.shaylee.netty.client.controller;
 import com.shaylee.netty.client.config.NettyClient;
 import com.shaylee.netty.common.protocol.protobuf.MessageBase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -17,11 +18,11 @@ public class ConsumerController {
     @Autowired
     private NettyClient nettyClient;
 
-    @GetMapping("/send")
-    public String send() {
+    @RequestMapping("/send")
+    public String send(@RequestParam String content) {
         MessageBase.Message message = new MessageBase.Message()
                 .toBuilder().setCmd(MessageBase.Message.CommandType.NORMAL)
-                .setContent("hello netty")
+                .setContent(content)
                 .setRequestId(UUID.randomUUID().toString()).build();
         nettyClient.sendMsg(message);
         return "send ok";
